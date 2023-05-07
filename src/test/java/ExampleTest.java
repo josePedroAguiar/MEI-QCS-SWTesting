22ecdf4c8da92882a32c716f613bcb5e3a7d7c5b
José Pedro Aguiar <88104767+josePedroAguiar@users.noreply.github.com>
2023-05-07 14:59:24 +0100

    1st commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class ExampleTest {
+    @Test
+    void ShouldShowSimpleAssertion () {
+        Assertions.assertEquals ( 1, 1);
+    }
+}
